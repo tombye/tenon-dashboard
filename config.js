8b7979e0d10851df49f61c54509041194e999ce1
tombye <tombaromba@gmail.com>
2014-07-29 22:49:34 +0100

    Clean up creds usage & results display

--- config.js
+++ config.js
@@ -1,13 +1,6 @@
 window.config = {
   api : 'http://protected-ridge-2923.herokuapp.com',
-  defaultOpts : {
-    'certainty' : 0,
-    'importance' : 0,
-    'level' : 'AAA',
-    'priority' : 0,
-    'ref' : 1,
-
-  },
+  defaultOpts : {},
   pages : [
     {
       'url' : 'https://www.gov.uk/vat'
