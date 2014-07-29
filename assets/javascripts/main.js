52e00426833c9d04a73589230de131f33768b6e6
tombye <tombaromba@gmail.com>
2014-07-29 21:49:34 +0100

    Initial commit for github page

---
+++ assets/javascripts/main.js
@@ -0,0 +1,33 @@
+(function (root) {
+  "use strict";
+
+  var $ = root.jQuery,
+      getQueryVariable,
+      PageRequest,
+      tenonDashboard;
+
+  getQueryVariable = function (variable) {
+    var query = window.location.search.replace(/\/{1}$/, '');
+    var vars = query.substring(1).split("&");
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+      if (pair[0] === variable) {
+        return pair[1];
+      }
+    }
+    return false;
+  };
+
+
+  $(document).ready(function () {
+    var username = getQueryVariable('username');
+    var password = getQueryVariable('password');
+    if (!username || !password) { return; }
+    var basicRequest = new XMLHttpRequest();
+    basicRequest.onreadystatechange = function (XHRObj) { console.log(XHRObj.srcElement); }
+    basicRequest.open("get", "http://protected-ridge-2923.herokuapp.com", true, username, password);
+    basicRequest.withCredentials = true
+    basicRequest.send()
+  });
+})(window);
+
---
+++ config.js
@@ -0,0 +1,16 @@
+window.config = {
+  api : 'http://protected-ridge-2923.herokuapp.com',
+  defaultOpts : {
+    'certainty' : 0,
+    'importance' : 0,
+    'level' : 'AAA',
+    'priority' : 0,
+    'ref' : 1,
+
+  },
+  pages : [
+    {
+      'url' : 'https://www.gov.uk/vat'
+    }
+  ]
+};
